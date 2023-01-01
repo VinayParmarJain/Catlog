5a280abae9c73398785ec8255dc502e517296051
Vinay Parmar <vinay@cortexbci.com>
2023-01-02 01:11:33 +0530

    Learn : Flutter Widgets & Scaffold

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String name = "Cordie";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+        child: Text("Hello $name"),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catlog/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: SafeArea(
-          child: Container(
-            child: const Text("Hello World"),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: Home(),
     );
   }
 }
